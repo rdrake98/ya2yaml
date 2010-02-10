f6aeeae31055e7394cad756e1d92ce7b9290a3ec
Akira FUNAI <akira@funai.com>
2010-02-11 03:36:49 +0900

    backward compat (ruby <= 1.8.6): fix escape_as_utf8 again.
    avoid using String#bytes in ruby <= 1.8.6. (improving on 61a79f7)

--- lib/ya2yaml.rb
+++ lib/ya2yaml.rb
@@ -253,7 +253,9 @@ class Ya2YAML
 				when is_printable?(ucs_code)
 					c
 				when @options[:escape_as_utf8]
-					c.bytes.collect {|b| '\\x%.2x' % b }.join
+					c.respond_to?(:bytes) ?
+						c.bytes.collect {|b| '\\x%.2x' % b }.join :
+						'\\x' + c.unpack('H2' * c.size).join('\\x')
 				when ucs_code == 0x2028 || ucs_code == 0x2029
 					ESCAPE_SEQ_LB[c]
 				when ucs_code <= 0x7f
